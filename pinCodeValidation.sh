e408ca9b97ab40787b2ac7c748ae4a1b57473597
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 13:42:22 +0530

    ADDED:Regex to restrict alphabet at end

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -2,8 +2,8 @@
 
 shopt -s extglob
 
-read -p "Enter pin code starting with alphabet to check validation :" pinCode
-pinCodePattern="^[0-9]{6}$"
+read -p "Enter pin code ending with alphabet to test validation:" pinCode
+pinCodePattern="^[0-9]{5}\d$"
 	if [[ $pinCode =~ $pinCodePattern ]]
 	then
 		echo "Pincode is valid"
