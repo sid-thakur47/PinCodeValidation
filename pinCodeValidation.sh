d2f0bb047bb30ff4f2ef63a6228a465869ac6df1
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 13:54:27 +0530

    ADDED:Regex to allow space after 3 numbers

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 
-read -p "Enter pin code ending with alphabet to test validation:" pinCode
+read -p "Enter pin code:" pinCode
 pinCodePattern="^[0-9]{3}[ ][0-9]{2}\d$"
 	if [[ $pinCode =~ $pinCodePattern ]]
 	then
