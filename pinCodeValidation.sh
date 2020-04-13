679cf3bb32e13db267b9a5ebf2d4a16d28813476
sid-thakur47 <sidthakur258@gmail.com>
2020-04-13 16:11:50 +0530

    ADDED:Code(regex) to allow space as option

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 
 read -p "Enter pin code:" pinCode
-pinCodePattern="^[0-9]{3}[ ][0-9]{2}\d$"
+pinCodePattern="^[0-9]{3}[\s]?[0-9]{3}"
 	if [[ $pinCode =~ $pinCodePattern ]]
 	then
 		echo "Pincode is valid"
